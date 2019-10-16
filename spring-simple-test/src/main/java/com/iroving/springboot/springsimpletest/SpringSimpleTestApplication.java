ccdbdeee15435f7f9f491d6c0ae55d093f370d8d
iroving <962067750@qq.com>
2019-10-16 10:23:56 +0800

    提交项目文件

---
+++ spring-simple-test/src/main/java/com/iroving/springboot/springsimpletest/SpringSimpleTestApplication.java
@@ -0,0 +1,13 @@
+package com.iroving.springboot.springsimpletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSimpleTestApplication.class, args);
+    }
+
+}
---
+++ spring-simple-test/src/test/java/com/iroving/springboot/springsimpletest/SpringSimpleTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iroving.springboot.springsimpletest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSimpleTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
