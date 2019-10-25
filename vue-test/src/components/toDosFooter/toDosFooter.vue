ae7209a879261049195185cb59486be3542facf6
iroving <962067750@qq.com>
2019-10-25 14:30:08 +0800

    提交build文件夹

--- vue-test/src/components/toDosFooter/toDosFooter.vue
+++ vue-test/src/components/toDosFooter/toDosFooter.vue
@@ -1,19 +1,48 @@
 <template>
   <div class="todo-footer">
     <label>
-      <input type="checkbox"/>
+      <input type="checkbox" v-model="allFinished"/>
     </label>
     <span>
-          <span>已完成0</span> / 全部{{todoList.length}}
+          <span>已完成{{hasComplete}}</span> / 全部{{todoList.length}}
         </span>
     <button class="btn btn-danger">清除已完成任务</button>
   </div>
 </template>
 
 <script>
-    export default {
-        props: {
-          todoList:Array
+  export default {
+    props: {
+      todoList: Array
+    },
+    computed: {
+      hasComplete:{
+        get(){
+          return this.todoList.filter((value, index) => {
+            if (value.isCheck) {
+              return value;
+            }
+          }).length;
         },
-    };
+        set(){
+
+        }
+      },
+      allFinished: {
+        get(){
+          return this.todoList.length == this.hasComplete && this.todoList.length != 0;
+        },
+        set(value) {
+          if (value) {
+            this.todoList.forEach(x => x.isCheck = true);
+          } else {
+            this.todoList.forEach(x => x.isCheck = false);
+          }
+        }
+
+
+
+      }
+    }
+  };
 </script>
--- vue-test/src/components/toDosItem/toDosItem.vue
+++ vue-test/src/components/toDosItem/toDosItem.vue
@@ -2,7 +2,7 @@
   <div>
     <li v-for="(value,index) in todoList" :key="index">
       <label>
-        <input type="checkbox" :checked="value.isCheck"/>
+        <input type="checkbox" v-model="value.isCheck"/>
         <span>{{value.message}}</span>
       </label>
       <button class="btn btn-danger" style="display:none">删除</button>
