c5e8fd1f99937d0ff66038bc0bced541f04f70b8
longlu94@gmail.com <longlu.0>
2019-11-21 10:59:09 +0800

    屏蔽公司包

--- hip-springboot/src/main/java/hip/cda/hipcda/rpc/SystemRpcService.java
+++ hip-springboot/src/main/java/hip/cda/hipcda/rpc/SystemRpcService.java
@@ -1,24 +1,24 @@
-package hip.cda.hipcda.rpc;
-
-import com.google.common.collect.Maps;
-import ctd.annotation.RpcClass;
-import ctd.net.rpc.Client;
-import ctd.util.JSONUtils;
-import ctd.util.annotation.RpcService;
-
-import java.util.Map;
-
-/**
- * @author:
- * @date:
- * @description: 对外提供的rpc接口
- **/
-@RpcClass(serviceID="systemService")
-public class SystemRpcService {
-
-    @RpcService
-    public String quertSystem(Object param) throws Exception {
-        return "1";
-    }
-
-}
+//package hip.cda.hipcda.rpc;
+//
+//import com.google.common.collect.Maps;
+//import ctd.annotation.RpcClass;
+//import ctd.net.rpc.Client;
+//import ctd.util.JSONUtils;
+//import ctd.util.annotation.RpcService;
+//
+//import java.util.Map;
+//
+///**
+// * @author:
+// * @date:
+// * @description: 对外提供的rpc接口
+// **/
+//@RpcClass(serviceID="systemService")
+//public class SystemRpcService {
+//
+//    @RpcService
+//    public String quertSystem(Object param) throws Exception {
+//        return "1";
+//    }
+//
+//}
