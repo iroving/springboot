103646fe279a3ec8a6a1caebf2d1cc528e6e2ad6
longlu94@gmail.com <longlu.0>
2019-11-21 10:55:12 +0800

    新增一个集成一些插件的项目  支持打war包

--- spring-simple-test/src/main/java/com/iroving/springboot/springsimpletest/SpringSimpleTestApplication.java
+++ spring-simple-test/src/main/java/com/iroving/springboot/springsimpletest/SpringSimpleTestApplication.java
@@ -2,7 +2,10 @@ package com.iroving.springboot.springsimpletest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringSimpleTestApplication {
 
@@ -10,4 +13,8 @@ public class SpringSimpleTestApplication {
         SpringApplication.run(SpringSimpleTestApplication.class, args);
     }
 
+    @RequestMapping("/")
+    public String index(){
+        return "Hello Spring Boot";
+    }
 }
